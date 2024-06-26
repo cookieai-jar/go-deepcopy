1db12942d939889bc9cb25511e829aff634d373b
Dejan Lokar <dlokar@veza.com>
2024-06-26 22:30:41 +0200

    Cherry pick fix for time.Time
    Original commit https://github.com/barkimedes/go-deepcopy/commit/5ad10c11cc597897a044ccd50a40bf58a689a08f

--- deepcopy_test.go
+++ deepcopy_test.go
@@ -1,9 +1,12 @@
-package deepcopy
+package deepcopy_test
 
 import (
 	"fmt"
 	. "reflect"
 	"testing"
+	"time"
+
+	"github.com/cookieai-jar/go-deepcopy"
 )
 
 func ExampleAnything() {
@@ -26,7 +29,7 @@ func ExampleAnything() {
 	}
 
 	for _, expected := range tests {
-		actual := MustAnything(expected)
+		actual := deepcopy.MustAnything(expected)
 		fmt.Println(actual)
 	}
 	// Output:
@@ -49,7 +52,7 @@ func ExampleMap() {
 		"foo": &Foo{Bar: 1},
 		"bar": &Foo{Bar: 2},
 	}
-	y := MustAnything(x).(map[string]*Foo)
+	y := deepcopy.MustAnything(x).(map[string]*Foo)
 	for _, k := range []string{"foo", "bar"} { // to ensure consistent order
 		fmt.Printf("x[\"%v\"] = y[\"%v\"]: %v\n", k, k, x[k] == y[k])
 		fmt.Printf("x[\"%v\"].Foo = y[\"%v\"].Foo: %v\n", k, k, x[k].Foo == y[k].Foo)
@@ -66,12 +69,12 @@ func ExampleMap() {
 
 func TestInterface(t *testing.T) {
 	x := []interface{}{nil}
-	y := MustAnything(x).([]interface{})
+	y := deepcopy.MustAnything(x).([]interface{})
 	if !DeepEqual(x, y) || len(y) != 1 {
 		t.Errorf("expect %v == %v; y had length %v (expected 1)", x, y, len(y))
 	}
 	var a interface{}
-	b := MustAnything(a)
+	b := deepcopy.MustAnything(a)
 	if a != b {
 		t.Errorf("expected %v == %v", a, b)
 	}
@@ -82,7 +85,7 @@ func ExampleAvoidInfiniteLoops() {
 		Bar: 4,
 	}
 	x.Foo = x
-	y := MustAnything(x).(*Foo)
+	y := deepcopy.MustAnything(x).(*Foo)
 	fmt.Printf("x == y: %v\n", x == y)
 	fmt.Printf("x == x.Foo: %v\n", x == x.Foo)
 	fmt.Printf("y == y.Foo: %v\n", y == y.Foo)
@@ -102,7 +105,7 @@ func TestUnsupportedKind(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		y, err := Anything(test)
+		y, err := deepcopy.Anything(test)
 		if y != nil {
 			t.Errorf("expected %v to be nil", y)
 		}
@@ -119,7 +122,7 @@ func TestUnsupportedKindPanicsOnMust(t *testing.T) {
 		}
 	}()
 	x := func() {}
-	MustAnything(x)
+	deepcopy.MustAnything(x)
 }
 
 func TestMismatchedTypesFail(t *testing.T) {
@@ -137,7 +140,7 @@ func TestMismatchedTypesFail(t *testing.T) {
 		},
 	}
 	for _, test := range tests {
-		for kind, copier := range copiers {
+		for kind, copier := range deepcopy.Copiers {
 			if kind == test.kind {
 				continue
 			}
@@ -160,16 +163,56 @@ func TestCopyNilValue(t *testing.T) {
 	type Foo struct {
 		Bar *Bar
 	}
-
 	s := &Foo{
 		Bar: nil,
 	}
-	c, err := Anything(s)
+	c, err := deepcopy.Anything(s)
 	if err != nil {
 		t.Fatalf("deepcopy of struct %#v failed", s)
 	}
 
 	if !DeepEqual(s, c) {
 		t.Fatalf("original and copied struct are not equal: %+v != %+v", s, c)
+	}
+}
+
+func TestTimeType(t *testing.T) {
+	src := time.Date(2016, 1, 1, 1, 0, 0, 0, time.UTC)
+	dst, err := deepcopy.Anything(src)
+	if err != nil {
+		t.Errorf("expected no error; got %v", err)
+	}
+	resultTime, ok := dst.(time.Time)
+	if !ok {
+		t.Errorf("expected a time.Time; got %v", resultTime)
+	}
+	if !DeepEqual(src, dst) {
+		t.Errorf("expect %v == %v; ", src, dst)
+	}
+
+}
+
+func TestTimePtrType(t *testing.T) {
+	type Foo struct {
+		T    time.Time
+		TPtr *time.Time
+	}
+
+	aTime := time.Date(2016, 1, 1, 1, 0, 0, 0, time.UTC)
+	anotherTime := aTime.Add(24 * time.Hour)
+	src := Foo{
+		T:    aTime,
+		TPtr: &anotherTime,
+	}
+	dst, err := deepcopy.Anything(src)
+	if err != nil {
+		t.Errorf("expected no error; got %v", err)
+	}
+	res, ok := dst.(Foo)
+	if !ok {
+		t.Errorf("expected a time.Time; got %v", res)
+	}
+	if !DeepEqual(src, dst) {
+		t.Errorf("expect %v == %v; ", src, dst)
 	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cookieai-jar/go-deepcopy
+
+go 1.22.4
